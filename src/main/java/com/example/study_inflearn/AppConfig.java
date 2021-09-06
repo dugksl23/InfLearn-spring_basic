1d32cc9023eb3e47766746fd6de1e323c4d14d1d
dugksl23 <dugksl23@gmail.com>
2021-09-06 20:11:46 +0900

    feat : AppConfig 를 통해 역할 및 역할에 따른 구현의 클래스를 들을 각각 명확히 구분하여 리팩토링 완료.

--- src/main/java/com/example/study_inflearn/AppConfig.java
+++ src/main/java/com/example/study_inflearn/AppConfig.java
@@ -1,6 +1,7 @@
 package com.example.study_inflearn;
 
 import com.example.study_inflearn.hello_core.Order.OrderServiceImpl;
+import com.example.study_inflearn.hello_core.discount.DiscountPolicy;
 import com.example.study_inflearn.hello_core.discount.FixDiscountPolicy;
 import com.example.study_inflearn.hello_core.member.MemberService;
 import com.example.study_inflearn.hello_core.member.MemberServiceImpl;
@@ -8,22 +9,37 @@ import com.example.study_inflearn.hello_core.member.MemoryMemberRepository;
 
 public class AppConfig {
 
+    //* 역할과 구현을 분리했으며, config에서는 각각의 각각의 역할에 따른 구현 캘르가 모두 드러나야 한다.
+    //  -> 나중에 jpa를 통해 db를 교체할 때는 함수별 역할만 코드를 수정하면 된다.
+
+
+    // 멤버 서비스 역할
     public MemberService memberService(){
 
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(getMemberRepository());
 
     }
 
+    // 멤버 repositorty 역할
+    private MemoryMemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    // order service 역할
     public OrderServiceImpl orderService(){
 
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(getMemberRepository(), getDiscountPolicy());
         // 20.08.23 (월(
         // new OrderServiceImpl()의 인터페이스는 부모 클래스로서, 구현체인 자식 클래스를 담을 수 있기에
         // 다형성을 통해서 return type이 OrderService 또는 OrderServiceImpl 모두 가능하다.
         // 그러나 구현체에 의존하지 않고, 역할인 interface와 의존관계를 갖기에 DIP를 지키고 있다.
-        // 되는 OCP의 원칙에 따라서 interface를 권장한다.
+        // 그리고 OCP의 원칙에 따라서 interface를 권장한다.
         // 또한, OrderServiceImpl의 의존관계인 interface들은 구현체 class중 어떤 것들이 들어올지 모른다.
         // 그렇기에 OCP에도 지키고 있다.
+    }
+
+    public DiscountPolicy getDiscountPolicy(){
+        return new FixDiscountPolicy();
     }
 
 }
