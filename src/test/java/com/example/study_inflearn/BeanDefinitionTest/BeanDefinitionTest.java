a39555a57204268135bc7369680994372be17e00
dugksl23 <dugksl23@gmail.com>
2021-09-27 23:45:04 +0900

    feat : BeanDefinition 인터페이스는 xml 및 app.class 의 java code 의 설정(meta) 정보를 통해 생성되며, 등록된 BeanDefinition을 추상화하여 제공

---
+++ src/test/java/com/example/study_inflearn/BeanDefinitionTest/BeanDefinitionTest.java
@@ -0,0 +1,26 @@
+package com.example.study_inflearn.BeanDefinitionTest;
+
+import com.example.study_inflearn.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BeanDefinitionTest {
+
+    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈설정 메타 정보 확인")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println("method : " + beanDefinitionName + ", type : " + ac.getBean(beanDefinitionName));
+        }
+    }
+
+
+
+
+}
--- src/test/java/com/example/study_inflearn/XML/XmlApplicationContext.java
+++ src/test/java/com/example/study_inflearn/XML/XmlApplicationContext.java
@@ -4,6 +4,7 @@ import com.example.study_inflearn.hello_core.member.Member;
 import com.example.study_inflearn.hello_core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class XmlApplicationContext {
@@ -14,6 +15,7 @@ public class XmlApplicationContext {
         MemberService memberService = ac.getBean("memberService", MemberService.class);
         Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
         // xml에서 등록된 bean들의 참조 class는 모두 구현체여야 한다.
+        new AnnotationConfigApplicationContext();
 
     }
 
