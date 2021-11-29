876ff5de24d6493571138d29a0206aa05a4abe04
dugksl23 <dugksl23@gmail.com>
2021-11-29 15:37:27 +0900

    feat : rollover time unit test

--- src/test/java/com/example/study_inflearn/BeanLifeCycle/LifeCycleTest.java
+++ src/test/java/com/example/study_inflearn/BeanLifeCycle/LifeCycleTest.java
@@ -6,6 +6,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class LifeCycleTest {
 
     @Test
@@ -17,7 +21,6 @@ public class LifeCycleTest {
 
     }
 
-
     @Configuration
     static class LifeCycleApp {
 
@@ -27,6 +30,27 @@ public class LifeCycleTest {
             client.setUrl("hi");
             return client;
         }
+
+    }
+
+
+
+    @Test
+    void rolloverTest(){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        cal.set(Calendar.MILLISECOND, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MINUTE, 0);
+
+
+        cal.add(Calendar.HOUR, 1);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+        String str = sdf.format(cal.getTimeInMillis());
+
+        System.out.println(" time : " + str);
+
 
     }
 }
